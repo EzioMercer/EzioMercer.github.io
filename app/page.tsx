67d2c26755c4c18b4152d838b2993d4d473f3e0f
EzioMercer <rovsanbadirxanov@gmail.com>
2024-09-01 15:46:51 +0400

    fix viewport height for mobile

--- app/page.tsx
+++ app/page.tsx
@@ -1,8 +1,26 @@
+'use client';
+
 import Header from './_components/Header/Header';
 import Footer from './_components/Footer/Footer';
 import Main from './_components/Main/Main';
+import { Viewport } from 'next';
+import { useEffect } from 'react';
+
+export const viewport: Viewport = {
+	width: 'device-width',
+	interactiveWidget: 'resizes-content',
+	initialScale: 1,
+	maximumScale: 1
+}
+
+const handleResize = () => {
+	document.documentElement.style.setProperty('--device-height', window.innerHeight + 'px');
+}
 
 const Page = () => {
+
+	useEffect(handleResize, []);
+
 	return (
 		<>
 			<Header />
