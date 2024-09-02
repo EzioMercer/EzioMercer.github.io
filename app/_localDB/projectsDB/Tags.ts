59d5e8e161af32a2ecfe691977f9c5bd3b43f150
EzioMercer <rovsanbadirxanov@gmail.com>
2024-09-03 01:38:08 +0400

    redesign search form

--- app/_localDB/projectsDB/Tags.ts
+++ app/_localDB/projectsDB/Tags.ts
@@ -5,18 +5,9 @@ const TAGS = {
 	HTML: 'HTML',
 	CSS: 'CSS',
 	JS: 'JS',
-	ALGORITHMS: 'Algorithm',
+	ALGORITHMS: 'Algorithms',
 	DATA_STRUCTURE: 'Data structure',
 	LIBRARY: 'Library',
-
-	LAYOUT_1: 'layout',
-	REACT_1: 'React',
-	CANVAS_1: 'canvas',
-	HTML_1: 'HTML',
-	CSS_1: 'CSS',
-	JS_1: 'JS',
-	ALGORITHM_1: 'Algorithm',
-	DATA_STRUCTURE_1: 'Data structure',
 } as const;
 
 export default TAGS;
