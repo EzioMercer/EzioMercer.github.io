4e9433259fbb4281325cb2d9606433b025424b46
EzioMercer <rovsanbadirxanov@gmail.com>
2024-09-05 02:47:31 +0400

    Add zoom out effect on text

--- app/_components/HeroSection/HeroSection.tsx
+++ app/_components/HeroSection/HeroSection.tsx
@@ -1,11 +1,21 @@
 import styles from './HeroSection.module.scss';
 import Container from '../Container/Container';
 
+const text = 'Welcome to navigation through EzioMercer\'s projects';
+
+const spans = text
+	.split(' ')
+	.map(word => <><span className={ styles.word }>{
+		word
+			.split('')
+			.map(symbol => <span className={ styles.symbol }>{ symbol }</span>)
+	}</span>{ ' ' }</>)
+
 const HeroSection = () => (
 	<section className={ styles.section }>
 		<Container className={ styles.container }>
-			<img src="/logo/EzioMercerLogo.png" alt="logo" width="128" height="128" />
-			<span>Welcome to navigation through EzioMercer's projects</span>
+			<img src="/logo/EzioMercerLogo.png" alt="logo" width="200" height="200" />
+			<span>{ spans }</span>
 			<span className={ styles.arrow }>↓</span>
 		</Container>
 	</section>
